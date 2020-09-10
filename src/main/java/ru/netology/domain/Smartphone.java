a9204743885365889872a89ec18b58aeb27fd2cb
Olesia D <osd0581@gmail.com>
2020-09-10 17:44:42 +0200

    product added

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,26 @@
+package ru.netology.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Smartphone extends Product {
+    private String vendor;
+
+    public Smartphone(int id, String name, int price, String vendor) {
+        super(id, name, price);
+        this.vendor = vendor;
+    }
+
+    @Override
+    public boolean matches(String search) {
+        if (super.matches(search)) {
+            return true;
+        }
+        if (search.equalsIgnoreCase(vendor)) {
+            return true;
+        }
+        return false;
+    }
+}
